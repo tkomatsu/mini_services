4ef4c9b39366748b72a85173c63d607925c138a5
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-26 13:17:59 +0900

    add metallb config and nginx service

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 minikube start --driver=docker
 eval $(minikube -p minikube docker-env)
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl apply -f metallb-configmap.yaml
+
 docker build -t nginx nginx
 kubectl apply -f nginx/nginx-deployment.yaml
+
+kubectl apply -f nginx/nginx-service.yaml
