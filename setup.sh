c709c5569a89026c77b8f597f28e39858e6a1e5c
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-28 22:30:04 +0900

    add secrets

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 kubectl apply -f metallb-configmap.yaml
+kubectl apply -f mini-secret.yaml
 
 docker build -t nginx nginx
 kubectl apply -f nginx/nginx-deployment.yaml
